8ab5cd2336a3601d68628b3ca748df21d6ae0157
Hubert Pompecki <hpompecki@gmail.com>
2015-11-22 17:02:32 +0000

    refractor storing feedback and implement printing board to the screen

--- lib/board.rb
+++ lib/board.rb
@@ -1,10 +1,11 @@
 class Board
   
-  attr_reader :secret_code, :total_attempts, :submitted_codes
+  attr_reader :secret_code, :total_attempts, :submitted_codes, :lines_of_feedback
 
   def initialize(total_attempts)
     @total_attempts = total_attempts
     @submitted_codes = []
+    @lines_of_feedback = []
   end
 
   def set_secret_code(secret_code)
@@ -13,10 +14,7 @@ class Board
 
   def submit_code(code)
     @submitted_codes << code
-  end
-
-  def get_feedback
-    @submitted_codes.last.generate_feedback(secret_code)
+    @lines_of_feedback << code.generate_feedback(secret_code)
   end
 
   def attempts_left
@@ -34,5 +32,20 @@ class Board
   def clear
     @secret_code = nil
     @submitted_codes = []
+    @lines_of_feedback = []
   end
+
+  def print_board
+    submitted_codes.each_with_index { |code, index| print_board_line(code, lines_of_feedback[index]) }
+  end
+
+  private
+
+    def print_board_line(code, feedback)
+      puts "#{code} | #{print_feedback(feedback)}"
+    end
+
+    def print_feedback(feedback)
+      "black pins: #{feedback[:black]}, white pins: #{feedback[:white]}"
+    end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,6 +3,10 @@ require "spec_helper"
 RSpec.describe Board do
 
   let(:board) { Board.new(12) }
+  
+  def sample_code(id = 0)
+    double(id, generate_feedback: {black: 0, white: 4}, to_s: "code #{id}")
+  end
 
   context "#initialize" do
     it "is initialized with the number of attempts" do
@@ -19,30 +23,20 @@ RSpec.describe Board do
 
   context "#submit_code" do
     it "puts a new code guess on board" do
-      board.submit_code(1)
-      board.submit_code(2)
-      board.submit_code(3)
+      board.submit_code(sample_code(1))
+      board.submit_code(sample_code(2))
+      board.submit_code(sample_code(3))
       expect(board.submitted_codes.length).to eq 3
     end
   end
 
   context "#submitted_codes" do
     it "returns an array of submitted codes" do
-      board.submit_code("a")
-      board.submit_code("b")
-      board.submit_code("c")
-      expect(board.submitted_codes).to eq ["a", "b", "c"]
-    end
-  end
-
-  context "#get_feedback" do
-    it "gets feedback for the secret" do
-      secret = double("secret")
-      guess = double("guess")
-      board.set_secret_code(secret)
-      board.submit_code(guess)
-      expect(guess).to receive(:generate_feedback).with(secret)
-      board.get_feedback
+      a, b, c = sample_code(1), sample_code(2), sample_code(3)
+      board.submit_code(a)
+      board.submit_code(b)
+      board.submit_code(c)
+      expect(board.submitted_codes).to eq [a, b, c]
     end
   end
 
@@ -55,32 +49,32 @@ RSpec.describe Board do
 
   context "#attempts_left" do
     it "returns the number of attempts left" do
-      3.times { board.submit_code("a") }
+      3.times { board.submit_code(sample_code) }
       expect(board.attempts_left).to eq 9
     end
   end
 
   context "#attempts_left?" do
     it "returns true if attempts left on board" do
-      11.times { board.submit_code("attempt") }
+      11.times { board.submit_code(sample_code) }
       expect(board.attempts_left?).to be true
     end
     it "returns false if no attempts left on board" do
-      12.times { board.submit_code("attempt") }
+      12.times { board.submit_code(sample_code) }
       expect(board.attempts_left?).to be false
     end
   end
 
   context "#code_guessed?" do
     it "returns true if last guessed code matches the secret" do
-      matching_code = double("Code", matches?: true)
-      4.times { board.submit_code("wrong guess")}
+      matching_code = double("Code", generate_feedback: nil, matches?: true)
+      4.times { board.submit_code(sample_code("wrong guess"))}
       board.submit_code(matching_code)
       expect(board.code_guessed?).to be true
     end
     it "returns false if last guessed code does not match the secret" do
-      not_matching_code = double("Code", matches?: false)
-      2.times { board.submit_code("wrong guess")}
+      not_matching_code = double("Code", generate_feedback: nil, matches?: false)
+      2.times { board.submit_code(sample_code("wrong guess"))}
       board.submit_code(not_matching_code)
       expect(board.code_guessed?).to be false
     end
@@ -88,14 +82,36 @@ RSpec.describe Board do
 
   context "#clear" do
     it "deletes all submitted codes" do
-      4.times { board.submit_code("some guess")}
+      4.times { board.submit_code(sample_code)}
       board.clear
       expect(board.submitted_codes.length).to eq 0
     end
+    it "deletes all lines of feedback" do
+      4.times { board.submit_code(sample_code)}
+      board.clear
+      expect(board.lines_of_feedback.length).to eq 0
+    end
     it "deletes the secret code" do
-      board.set_secret_code("secret")
+      board.set_secret_code(sample_code("secret"))
       board.clear
       expect(board.secret_code).to be_nil
+    end
+  end
+
+  context "#print_board" do
+    it "print a board with one line to the screen" do
+      board.submit_code(sample_code(0))
+      expect(STDOUT).to receive(:puts).with("code 0 | black pins: 0, white pins: 4")
+      board.print_board
+    end
+    it "print a board with multiple lines to the screen" do
+      board.submit_code(sample_code(0))
+      board.submit_code(sample_code(1))
+      board.submit_code(sample_code(2))
+      expect(STDOUT).to receive(:puts).with("code 0 | black pins: 0, white pins: 4")
+      expect(STDOUT).to receive(:puts).with("code 1 | black pins: 0, white pins: 4")
+      expect(STDOUT).to receive(:puts).with("code 2 | black pins: 0, white pins: 4")
+      board.print_board
     end
   end
 end
