3b9870abd8a42d7c11a54fee05ae0b3f77c4933a
Hubert Pompecki <hpompecki@gmail.com>
2015-11-29 08:48:09 +0000

    bug fix

--- game_exec.rb
+++ game_exec.rb
@@ -3,12 +3,12 @@ Dir["lib/*.rb"].each { |file| require_relative file }
 puts "Enter the first player's name, the player will start as the codemaker:"
 name1 = gets.chomp
 puts "Is the player AI controlled? (Y)"
-ai1 = gets.chomp.upcase == 'Y'
+ai1 = (gets.chomp.upcase == 'Y') ? :AI : false
 
 puts "Enter the second player's name, the player will start as the codebreaker:"
 name2 = gets.chomp
 puts "Is the player AI controlled? (Y)"
-ai2 = gets.chomp.upcase == 'Y'
+ai2 = (gets.chomp.upcase == 'Y') ? :AI : false
 
 player1 = Player.new(name1, ai1)
 player2 = Player.new(name2, ai2)
